c64810a5b69a94c85a1dd7c3750bb16263c83f4a
Yass <122342504+Yass-Seer@users.noreply.github.com>
2024-09-13 12:54:32 +1000

    chore: Clean up typing, formatting, etc

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,44 @@
 import base64
+from dataclasses import dataclass, field
+from typing import Dict, Literal
+
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 
-def format_image_url_prompt(image):
-    """_summary_
+@dataclass
+class ImageUrlData:
+    url: str
+
+
+@dataclass
+class ImageUrlResponse:
+    image_url: ImageUrlData
+    type: Literal["image_url"] = field(default="image_url")
+
+
+def format_image_url_prompt(image: UploadedFile) -> ImageUrlResponse:
+    """Converts an image to a base64 encoded string and returns a dictionary with the image URL.
 
     Args:
-        image (Streamlit UploadedFile): See
+        image (UploadedFile): See
             - https://docs.streamlit.io/develop/api-reference/widgets/st.file_uploader
     """
     image_type = image.type
-    # bytes_data = image.getvalue()
     base64_image = get_base64_encoded_image(image.getvalue())
 
-    return {
-        "type": "image_url",
-        "image_url": {"url": f"data:{image_type};base64,{base64_image}"},
-    }
+    url = f"data:{image_type};base64,{base64_image}"
+    return ImageUrlResponse(image_url=ImageUrlData(url=url))
 
 
-def get_base64_encoded_image(image_bytes):
+def get_base64_encoded_image(image_bytes: bytes) -> str:
+    """Convert an image (as bytes) to a base64 encoded string.
+
+    Args:
+        image_bytes (bytes): Bytes representation of an image
+
+    Returns:
+        str: Images as a base64 encoded string
+    """
     base_64_encoded_data = base64.b64encode(image_bytes)
     base64_string = base_64_encoded_data.decode("utf-8")
     return base64_string
